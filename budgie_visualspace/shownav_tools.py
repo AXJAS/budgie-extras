1f01a141a3b8a702dc9ecdde6aac443f22832122
Jacob-Vlijm <vlijm@planet.nl>
2018-02-10 00:53:30 +0100

    Added license info.

--- budgie_visualspace/shownav_tools.py
+++ budgie_visualspace/shownav_tools.py
@@ -5,6 +5,22 @@ import gi
 from gi.repository import Gio
 
 
+"""
+VisualSpace
+Author: Jacob Vlijm
+Copyright © 2017-2018 Ubuntu Budgie Developers
+Website=https://ubuntubudgie.org
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along with this
+program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 navpath = os.path.dirname(os.path.abspath(__file__))
 navigator = os.path.join(navpath, "shownav")
 user = os.environ["USER"]
